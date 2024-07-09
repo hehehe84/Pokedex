f197e04665c45051fe32b160678daa50e1897796
hehehe84 <antoine.picott@gmail.com>
2024-07-09 16:23:20 +0200

    main is inside src => TypeScript build in the dist => main.js in build and Heroku deployment should be good

--- dist/main.js
+++ dist/main.js
@@ -1,34 +1,25 @@
-const express = require('express')
-const favicon = require('serve-favicon')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 // const handle404 = require('./src/middlewares/handle404');
-const setupRoutes = require('./dist/routes/router').default;
-
+const setupRoutes = require('./routes/router').default;
 const app = express();
 const port = process.env.PORT || 3000;
-
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(bodyParser.json())
-    .use(cors())
-
-
+    .use(cors());
 //Production get
-
 app.get('/', (req, res) => {
     res.json('Hello Heroku Server!');
-})
-
+});
 //Router
 setupRoutes(app);
 // app.use(({res}) => )
-
 //404
-app.use(({res}) => {
+app.use(({ res }) => {
     const message = "Impossible to find this Page. Try another URL";
-    res.status(404).json({message});
+    res.status(404).json({ message });
 });
-
-app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
-
+app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`));
